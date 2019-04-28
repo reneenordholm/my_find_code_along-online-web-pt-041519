87346355df40dedfb7c0648a01246c8751319414
Renee Nordholm <reneenordholm@gmail.com>
2019-04-28 00:50:10 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -5,10 +5,5 @@ def my_find(collection)
   while i < collection.length
     return collection[i] if yield(collection[i])
     i = i + 1
-=======
-    while i < collection.length
-      yield collection[i]
-    i += 0
->>>>>>> bd46ef992303eaa51cb5b6497df546f3b3c49649
   end
 end
